1113bed116baa75c1639b20446b34b66bbf2e8f4
SebiXonX <69300981+SebiXonX@users.noreply.github.com>
2021-04-08 14:05:06 +0200

    Delete windows/Python directory

--- windows/Python/Updater.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def Search():
-    subprocess.call('cd bin', shell=True)
-    ver = "git clone "
-    subprocess.call(ver, shell=True)
-    import Config
-    if Config.Version == Config.NewVersion:
-        print("Not update a")
